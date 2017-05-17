1eae833d66a2e8871ba191dc5c29683217519ffb
Payam Naderi <naderi.payam@gmail.com>
2017-05-17 13:57:20 +0430

    init module action aware

--- src/TenderBin/Module.php
+++ src/TenderBin/Module.php
@@ -1,117 +1,147 @@
 <?php
-namespace Module\TenderBin;
-
-use Poirot\Application\Interfaces\Sapi;
-use Poirot\Application\ModuleManager\Interfaces\iModuleManager;
-use Poirot\Application\Sapi\Module\ContainerForFeatureActions;
-use Poirot\Ioc\Container;
-use Poirot\Ioc\Container\BuildContainer;
-use Poirot\Router\BuildRouterStack;
-use Poirot\Router\Interfaces\iRouterStack;
-use Poirot\Std\Interfaces\Struct\iDataEntity;
-
-
-class Module implements Sapi\iSapiModule
-    , Sapi\Module\Feature\iFeatureModuleInitModuleManager
-    , Sapi\Module\Feature\iFeatureModuleMergeConfig
-    , Sapi\Module\Feature\iFeatureModuleNestServices
-    , Sapi\Module\Feature\iFeatureOnPostLoadModulesGrabServices
-    , Sapi\Module\Feature\iFeatureModuleNestActions
+namespace Module\TenderBin
 {
-    const CONF_KEY = 'module.tenderbin';
+    use Poirot\Application\Interfaces\Sapi;
+    use Poirot\Application\ModuleManager\Interfaces\iModuleManager;
+    use Poirot\Application\Sapi\Module\ContainerForFeatureActions;
+    use Poirot\Ioc\Container;
+    use Poirot\Ioc\Container\BuildContainer;
+    use Poirot\Router\BuildRouterStack;
+    use Poirot\Router\Interfaces\iRouterStack;
+    use Poirot\Std\Interfaces\Struct\iDataEntity;
 
 
-    /**
-     * Initialize Module Manager
-     *
-     * priority: 1000 C
-     *
-     * @param iModuleManager $moduleManager
-     *
-     * @return void
-     */
-    function initModuleManager(iModuleManager $moduleManager)
+    class Module implements Sapi\iSapiModule
+        , Sapi\Module\Feature\iFeatureModuleInitModuleManager
+        , Sapi\Module\Feature\iFeatureModuleMergeConfig
+        , Sapi\Module\Feature\iFeatureModuleNestServices
+        , Sapi\Module\Feature\iFeatureOnPostLoadModulesGrabServices
+        , Sapi\Module\Feature\iFeatureModuleNestActions
     {
-        // ( ! ) ORDER IS MANDATORY
+        const CONF_KEY = 'module.tenderbin';
 
-        if (!$moduleManager->hasLoaded('MongoDriver'))
-            // MongoDriver Module Is Required.
-            $moduleManager->loadModule('MongoDriver');
 
-        if (!$moduleManager->hasLoaded('OAuth2Client'))
-            // Load OAuth2 Client To Assert Tokens.
-            $moduleManager->loadModule('OAuth2Client');
-    }
+        /**
+         * Initialize Module Manager
+         *
+         * priority: 1000 C
+         *
+         * @param iModuleManager $moduleManager
+         *
+         * @return void
+         */
+        function initModuleManager(iModuleManager $moduleManager)
+        {
+            // ( ! ) ORDER IS MANDATORY
 
-    /**
-     * Register config key/value
-     *
-     * priority: 1000 D
-     *
-     * - you may return an array or Traversable
-     *   that would be merge with config current data
-     *
-     * @param iDataEntity $config
-     *
-     * @return array|\Traversable
-     */
-    function initConfig(iDataEntity $config)
-    {
-        return \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin');
-    }
+            if (!$moduleManager->hasLoaded('MongoDriver'))
+                // MongoDriver Module Is Required.
+                $moduleManager->loadModule('MongoDriver');
 
-    /**
-     * Get Action Services
-     *
-     * priority not that serious
-     *
-     * - return Array used to Build ModuleActionsContainer
-     *
-     * @return array|ContainerForFeatureActions|BuildContainer|\Traversable
-     */
-    function getActions()
-    {
-        return \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin.actions');
-    }
+            if (!$moduleManager->hasLoaded('OAuth2Client'))
+                // Load OAuth2 Client To Assert Tokens.
+                $moduleManager->loadModule('OAuth2Client');
+        }
 
-    /**
-     * Get Nested Module Services
-     *
-     * it can be used to manipulate other registered services by modules
-     * with passed Container instance as argument.
-     *
-     * priority not that serious
-     *
-     * @param Container $moduleContainer
-     *
-     * @return null|array|BuildContainer|\Traversable
-     */
-    function getServices(Container $moduleContainer = null)
-    {
-        $conf = \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin.services');
-        return $conf;
-    }
+        /**
+         * Register config key/value
+         *
+         * priority: 1000 D
+         *
+         * - you may return an array or Traversable
+         *   that would be merge with config current data
+         *
+         * @param iDataEntity $config
+         *
+         * @return array|\Traversable
+         */
+        function initConfig(iDataEntity $config)
+        {
+            return \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin');
+        }
 
-    /**
-     * Resolve to service with name
-     *
-     * - each argument represent requested service by registered name
-     *   if service not available default argument value remains
-     * - "services" as argument will retrieve services container itself.
-     *
-     * ! after all modules loaded
-     *
-     * @param iRouterStack $router
-     */
-    function resolveRegisteredServices(
-        $router = null
-    ) {
-        # Register Http Routes:
-        if ($router) {
-            $routes = include __DIR__ . '/../../config/mod-tenderbin.routes.conf.php';
-            $buildRoute = new BuildRouterStack;
-            $buildRoute->setRoutes($routes);
-            $buildRoute->build($router);
+        /**
+         * Get Action Services
+         *
+         * priority not that serious
+         *
+         * - return Array used to Build ModuleActionsContainer
+         *
+         * @return array|ContainerForFeatureActions|BuildContainer|\Traversable
+         */
+        function getActions()
+        {
+            return \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin.actions');
+        }
+
+        /**
+         * Get Nested Module Services
+         *
+         * it can be used to manipulate other registered services by modules
+         * with passed Container instance as argument.
+         *
+         * priority not that serious
+         *
+         * @param Container $moduleContainer
+         *
+         * @return null|array|BuildContainer|\Traversable
+         */
+        function getServices(Container $moduleContainer = null)
+        {
+            $conf = \Poirot\Config\load(__DIR__ . '/../../config/mod-tenderbin.services');
+            return $conf;
+        }
+
+        /**
+         * Resolve to service with name
+         *
+         * - each argument represent requested service by registered name
+         *   if service not available default argument value remains
+         * - "services" as argument will retrieve services container itself.
+         *
+         * ! after all modules loaded
+         *
+         * @param iRouterStack $router
+         */
+        function resolveRegisteredServices(
+            $router = null
+        ) {
+            # Register Http Routes:
+            if ($router) {
+                $routes = include __DIR__ . '/../../config/mod-tenderbin.routes.conf.php';
+                $buildRoute = new BuildRouterStack;
+                $buildRoute->setRoutes($routes);
+                $buildRoute->build($router);
+            }
         }
     }
+}
+
+
+namespace Module\TenderBin\Actions
+{
+
+    /**
+     * @property RenderBinAction  $renderBinAction
+     * @property CreateBinAction  $createBinAction
+     * @property UpdateBinAction  $updateBinAction
+     * @property DeleteBinAction  $deleteBinAction
+     * @property SearchBinAction  $searchBinAction
+     * @property GetMetaBinAction $getMetaBinAction
+     *
+     */
+    class IOC extends \IOC
+    { }
+}
+
+
+namespace Module\Content\Services\Repository
+{
+    use Module\TenderBin\Model\Driver\Mongo\BindataRepo;
+
+    /**
+     * @method static BindataRepo Bindata()
+     */
+    class IOC extends \IOC
+    { }
 }
